73fe2545bbfea1c1964ff0757c54b6f12ea33c59
Atul Varma <varmaa@gmail.com>
2023-12-25 01:46:11 -0500

    Origination.

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::{error::Error, fmt::Display};
+
+#[derive(Debug)]
+struct SyntaxError {
+}
+
+impl Error for SyntaxError {}
+
+impl Display for SyntaxError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "SYNTAX ERROR")
+    }
+}
+
+#[derive(Debug)]
+enum Token {
+    Print,
+}
+
+struct Tokenizer {
+    string: String
+    index: usize
+}
+
+impl Tokenizer {
+    pub fn new(string: String) -> Self {
+        assert!(string.is_ascii());
+        Tokenizer { string, index: 0 }
+    }
+}
+
+impl Iterator for Tokenizer {
+    type Item = Result<Token, SyntaxError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+
+        if self.string.contains("PRINT") {
+            Some(Ok(Token::Print))
+        } else {
+            None
+        }
+    }
+}
+
+fn main() {
+    let mut tok = Tokenizer::new(String::from("PRINT \"HELLO WORLD\""));
+    println!("First token: {:?}", tok.next());
+}
