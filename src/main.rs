a88b441ce0f21e4395571f62f7ae48e3c7c01e7c
Atul Varma <varmaa@gmail.com>
2023-12-26 18:59:02 -0500

    Add interpreter loop.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,53 @@
 mod syntax_error;
 mod tokenizer;
 
+use rustyline::{error::ReadlineError, DefaultEditor};
 use tokenizer::Tokenizer;
 
-fn main() {
-    let tokenizer = Tokenizer::new("PRINT \"HELLO WORLD\"");
-    for token in tokenizer {
-        println!("Token: {:?}", token);
+const HISTORY_FILENAME: &'static str = ".interpreter-history.txt";
+
+fn run_interpreter() -> i32 {
+    let Ok(mut rl) = DefaultEditor::new() else {
+        eprintln!("Initializing DefaultEditor failed!");
+        return 1;
+    };
+
+    // Ignore the result, if it errors it's generally b/c the file doesn't exist.
+    let _ = rl.load_history(HISTORY_FILENAME);
+
+    loop {
+        let readline = rl.readline("] ");
+        match readline {
+            Ok(line) => {
+                if let Err(err) = rl.add_history_entry(line.as_str()) {
+                    eprintln!("WARNING: Failed to add history entry (${:?}).", err);
+                }
+                let tokenizer = Tokenizer::new(line);
+                for token in tokenizer {
+                    println!("Token: {:?}", token);
+                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                eprintln!("CTRL-C pressed, exiting.");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                break;
+            }
+            Err(err) => {
+                eprintln!("Error: {:?}", err);
+                return 1;
+            }
+        }
     }
+
+    // Ignore the result, if we fail no biggie.
+    let _ = rl.save_history(HISTORY_FILENAME);
+
+    return 0;
+}
+
+fn main() {
+    let exit_code = run_interpreter();
+    std::process::exit(exit_code);
 }
