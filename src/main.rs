9d201e74190977032ddf441bb72403dd98f8fd4e
Atul Varma <varmaa@gmail.com>
2024-01-03 16:32:56 -0500

    Use clap, factor out StdioInterpreter.

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ mod value;
 use std::io::{stdin, IsTerminal};
 use std::sync::mpsc::channel;
 
+use clap::Parser;
 use ctrlc;
 use interpreter::{Interpreter, InterpreterState};
 use rustyline::{error::ReadlineError, DefaultEditor};
@@ -21,159 +22,181 @@ use stdio_printer::StdioPrinter;
 
 const HISTORY_FILENAME: &'static str = ".interpreter-history.txt";
 
-fn break_interpreter(printer: &mut StdioPrinter, interpreter: &mut Interpreter) {
-    // TODO: Applesoft BASIC actually lets the user use "CONT" to resume
-    // program execution after a break or "STOP" statement, it'd be nice
-    // to support that. Instead, we're currently just stopping the program
-    // and preventing it from being resumed.
-    if let Some(line_number) = interpreter.stop_evaluating() {
-        printer.eprintln(format!("BREAK IN {}", line_number));
-    } else {
-        printer.eprintln("BREAK");
-    }
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct CliArgs {
+    source_filename: Option<String>,
 }
 
-fn load_source_file(interpreter: &mut Interpreter, filename: &String) -> Result<(), ()> {
-    let Ok(code) = std::fs::read_to_string(filename) else {
-        println!("ERROR READING FILE: {}", filename);
-        return Err(());
-    };
-    let lines = code.split('\n');
-    for (i, line) in lines.enumerate() {
-        let Some(first_char) = line.chars().next() else {
-            continue;
-        };
-        if !first_char.is_ascii_digit() {
-            eprintln!(
-                "WARNING: Line {} of '{}' is not a numbered line, ignoring it.",
-                i + 1,
-                filename
-            );
-            continue;
-        }
-        if let Err(err) = interpreter.start_evaluating(line) {
-            println!("{}", err);
-            return Err(());
-        }
-    }
-    Ok(())
+struct StdioInterpreter {
+    args: CliArgs,
+    printer: StdioPrinter,
+    interpreter: Interpreter,
 }
 
-fn run_interpreter(source_filename: Option<String>) -> i32 {
-    let (tx, rx) = channel();
-
-    ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
-        .expect("Error setting Ctrl-C handler.");
-
-    let Ok(mut rl) = DefaultEditor::new() else {
-        eprintln!("Initializing DefaultEditor failed!");
-        return 1;
-    };
-
-    // Ignore the result, if it errors it's generally b/c the file doesn't exist.
-    let _ = rl.load_history(HISTORY_FILENAME);
-
-    let mut interpreter = Interpreter::new();
-
-    let mut initial_command = None;
-
-    if let Some(filename) = source_filename {
-        if let Err(_) = load_source_file(&mut interpreter, &filename) {
-            return 1;
+impl StdioInterpreter {
+    fn new(args: CliArgs) -> Self {
+        StdioInterpreter {
+            args,
+            printer: StdioPrinter::new(),
+            interpreter: Interpreter::new(),
         }
-        initial_command = Some("RUN");
     }
 
-    let mut printer = StdioPrinter::new();
-
-    loop {
-        printer.print_buffered_output();
-        let readline = if let Some(command) = initial_command.take() {
-            Ok(command.to_string())
+    fn break_interpreter(&mut self) {
+        // TODO: Applesoft BASIC actually lets the user use "CONT" to resume
+        // program execution after a break or "STOP" statement, it'd be nice
+        // to support that. Instead, we're currently just stopping the program
+        // and preventing it from being resumed.
+        if let Some(line_number) = self.interpreter.stop_evaluating() {
+            self.printer.eprintln(format!("BREAK IN {}", line_number));
         } else {
-            rl.readline("] ")
+            self.printer.eprintln("BREAK");
+        }
+    }
+
+    fn load_source_file(&mut self, filename: &String) -> Result<(), ()> {
+        let Ok(code) = std::fs::read_to_string(filename) else {
+            println!("ERROR READING FILE: {}", filename);
+            return Err(());
         };
-        match readline {
-            Ok(line) => {
-                if let Err(err) = rl.add_history_entry(line.as_str()) {
-                    eprintln!("WARNING: Failed to add history entry (${:?}).", err);
-                }
-                loop {
-                    let result = match interpreter.get_state() {
-                        InterpreterState::Idle => interpreter.start_evaluating(&line),
-                        InterpreterState::Running => interpreter.continue_evaluating(),
-                        InterpreterState::AwaitingInput => {
-                            panic!("We should never be in this state at the beginning of the loop")
+        let lines = code.split('\n');
+        for (i, line) in lines.enumerate() {
+            let Some(first_char) = line.chars().next() else {
+                continue;
+            };
+            if !first_char.is_ascii_digit() {
+                eprintln!(
+                    "WARNING: Line {} of '{}' is not a numbered line, ignoring it.",
+                    i + 1,
+                    filename
+                );
+                continue;
+            }
+            if let Err(err) = self.interpreter.start_evaluating(line) {
+                println!("{}", err);
+                return Err(());
+            }
+        }
+        Ok(())
+    }
+
+    fn run(&mut self) -> i32 {
+        let (tx, rx) = channel();
+
+        ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
+            .expect("Error setting Ctrl-C handler.");
+
+        let Ok(mut rl) = DefaultEditor::new() else {
+            eprintln!("Initializing DefaultEditor failed!");
+            return 1;
+        };
+
+        // Ignore the result, if it errors it's generally b/c the file doesn't exist.
+        let _ = rl.load_history(HISTORY_FILENAME);
+
+        let mut initial_command = None;
+
+        if let Some(filename) = &self.args.source_filename.clone() {
+            if let Err(_) = self.load_source_file(&filename) {
+                return 1;
+            }
+            initial_command = Some("RUN");
+        }
+
+        loop {
+            self.printer.print_buffered_output();
+            let readline = if let Some(command) = initial_command.take() {
+                Ok(command.to_string())
+            } else {
+                rl.readline("] ")
+            };
+            match readline {
+                Ok(line) => {
+                    if let Err(err) = rl.add_history_entry(line.as_str()) {
+                        eprintln!("WARNING: Failed to add history entry (${:?}).", err);
+                    }
+                    loop {
+                        let result = match self.interpreter.get_state() {
+                            InterpreterState::Idle => self.interpreter.start_evaluating(&line),
+                            InterpreterState::Running => self.interpreter.continue_evaluating(),
+                            InterpreterState::AwaitingInput => {
+                                panic!(
+                                    "We should never be in this state at the beginning of the loop"
+                                )
+                            }
+                        };
+
+                        // Regardless of whether an error occurred, show any buffered output.
+                        if let Some(output) = self.interpreter.get_and_clear_output_buffer() {
+                            self.printer.print(output);
                         }
-                    };
 
-                    // Regardless of whether an error occurred, show any buffered output.
-                    if let Some(output) = interpreter.get_and_clear_output_buffer() {
-                        printer.print(output);
-                    }
-
-                    if rx.try_recv().is_ok() {
-                        break_interpreter(&mut printer, &mut interpreter);
-                        break;
-                    }
-
-                    if let Err(err) = result {
-                        printer.eprintln(err.to_string());
-                        if stdin().is_terminal() {
+                        if rx.try_recv().is_ok() {
+                            self.break_interpreter();
                             break;
-                        } else {
-                            // If we're not interactive, treat errors as fatal.
-                            return 1;
                         }
-                    }
 
-                    match interpreter.get_state() {
-                        InterpreterState::Idle => break,
-                        InterpreterState::Running => {}
-                        InterpreterState::AwaitingInput => {
-                            let prompt = format!("{}? ", printer.pop_buffered_output());
-                            let readline = rl.readline(&prompt);
-                            match readline {
-                                Ok(line) => {
-                                    interpreter.provide_input(line);
-                                }
-                                Err(ReadlineError::Interrupted) => {
-                                    break_interpreter(&mut printer, &mut interpreter);
-                                    break;
-                                }
-                                Err(ReadlineError::Eof) => {
-                                    return 0;
-                                }
-                                Err(err) => {
-                                    eprintln!("Error: {:?}", err);
-                                    return 1;
+                        if let Err(err) = result {
+                            self.printer.eprintln(err.to_string());
+                            if stdin().is_terminal() {
+                                break;
+                            } else {
+                                // If we're not interactive, treat errors as fatal.
+                                return 1;
+                            }
+                        }
+
+                        match self.interpreter.get_state() {
+                            InterpreterState::Idle => break,
+                            InterpreterState::Running => {}
+                            InterpreterState::AwaitingInput => {
+                                let prompt = format!("{}? ", self.printer.pop_buffered_output());
+                                let readline = rl.readline(&prompt);
+                                match readline {
+                                    Ok(line) => {
+                                        self.interpreter.provide_input(line);
+                                    }
+                                    Err(ReadlineError::Interrupted) => {
+                                        self.break_interpreter();
+                                        break;
+                                    }
+                                    Err(ReadlineError::Eof) => {
+                                        return 0;
+                                    }
+                                    Err(err) => {
+                                        eprintln!("Error: {:?}", err);
+                                        return 1;
+                                    }
                                 }
                             }
                         }
                     }
                 }
-            }
-            Err(ReadlineError::Interrupted) => {
-                printer.eprintln("CTRL-C pressed, exiting.");
-                break;
-            }
-            Err(ReadlineError::Eof) => {
-                break;
-            }
-            Err(err) => {
-                eprintln!("Error: {:?}", err);
-                return 1;
+                Err(ReadlineError::Interrupted) => {
+                    self.printer.eprintln("CTRL-C pressed, exiting.");
+                    break;
+                }
+                Err(ReadlineError::Eof) => {
+                    break;
+                }
+                Err(err) => {
+                    eprintln!("Error: {:?}", err);
+                    return 1;
+                }
             }
         }
+
+        // Ignore the result, if we fail no biggie.
+        let _ = rl.save_history(HISTORY_FILENAME);
+
+        return 0;
     }
-
-    // Ignore the result, if we fail no biggie.
-    let _ = rl.save_history(HISTORY_FILENAME);
-
-    return 0;
 }
 
 fn main() {
-    let exit_code = run_interpreter(std::env::args().nth(1));
+    let args = CliArgs::parse();
+    let mut interpreter = StdioInterpreter::new(args);
+    let exit_code = interpreter.run();
     std::process::exit(exit_code);
 }
