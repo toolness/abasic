dbad3221712ba7fba1dea2b22937464f8b6ee0e8
Atul Varma <varmaa@gmail.com>
2024-01-06 18:45:10 -0500

    Add front-end CSS, HTML from werewolves-and-wanderer.

--- abasic-web/main.ts
+++ abasic-web/main.ts
@@ -1,6 +1,18 @@
 import { default as wasm, JsInterpreter, JsInterpreterState, JsInterpreterOutputType } from "./pkg/abasic_web.js";
 
 wasm().then((module) => {
+    const a11yOutputEl = el_with_id('a11y-output');
+    const outputEl = el_with_id('output');
+    const promptEl = el_with_id('prompt');
+    const inputEl = el_with_id('input');
+    const formEl = el_with_id('form');
+
+    if (!(inputEl instanceof HTMLInputElement))
+        throw new Error("Expected inputEl to be an <input>");
+
+    if (!(formEl instanceof HTMLFormElement))
+        throw new Error("Expected formEl to be a <form>");
+
     let interpreter = JsInterpreter.new();
     interpreter.start_evaluating('PRINT "HELLO " 1+2');
     if (interpreter.get_state() === JsInterpreterState.Errored) {
@@ -12,3 +24,10 @@ wasm().then((module) => {
         console.log(JsInterpreterOutputType[item.output_type], item.into_string());
     }
 });
+
+function el_with_id(id: string): HTMLElement {
+    const el = document.getElementById(id);
+    if (el === null)
+        throw new Error(`Element with id "${id}" not found!`);
+    return el;
+}
