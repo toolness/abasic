48b9694736b8937d315eb1d28e7c3fbde3984a49
Atul Varma <varmaa@gmail.com>
2024-01-12 18:43:21 -0500

    Use transparent Debug impl for symbols.

--- abasic-core/src/symbol.rs
+++ abasic-core/src/symbol.rs
@@ -1,3 +1,4 @@
+use core::fmt::Debug;
 use std::{fmt::Display, rc::Rc};
 
 /// This is a newtype for a symbol (e.g. an identifier referencing the name
@@ -5,7 +6,7 @@ use std::{fmt::Display, rc::Rc};
 ///
 /// Using a newtype allows us to easily change the implementation without
 /// needing to change a bunch of dependent code.
-#[derive(Debug, PartialEq, Clone, Hash, Eq)]
+#[derive(PartialEq, Clone, Hash, Eq)]
 pub struct Symbol(Rc<String>);
 
 impl Symbol {
@@ -15,6 +16,12 @@ impl Symbol {
 }
 
 impl Display for Symbol {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Debug for Symbol {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
     }
